185d71c2b8c7231904ed7e86bc642db176528149
nghiahuunguyen <145564918+nghiahuunguyen@users.noreply.github.com>
2024-04-24 22:42:28 +0700

    .

---
+++ quanlycybergams/quanlycybergams/Controllers/TrangchuController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using quanlycybergams.Models;
+
+namespace quanlycybergams.Controllers
+{
+    public class TrangchuController : Controller
+    {
+        private QuanLyCYBERGAMESEntities db = new QuanLyCYBERGAMESEntities();
+
+        // GET: Trangchu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            var user = db.TaiKhoans.FirstOrDefault(u => u.TenDN == username && u.Matkhau == password);
+
+            if (user != null)
+            {
+                // Check if the user is an admin (assuming the admin user has a specific ID_TK value, e.g., 1)
+                if (user.ID_KhachHang == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                {
+                    // Handle non-admin user case
+                    // For example, redirect to a different page or display an error message
+                    ViewBag.ErrorMessage = "You are not authorized to access the admin area.";
+                    return View();
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid username or password";
+                return View();
+            }
+        }
+    }
+}
