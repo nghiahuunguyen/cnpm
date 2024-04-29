581e093997e448dbf76936997ef3186856f17c63
nghiahuunguyen <145564918+nghiahuunguyen@users.noreply.github.com>
2024-04-29 20:10:11 +0700

    .,

---
+++ quanlycybergams/quanlycybergams/Areas/Admin/Controllers/DonHangsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using quanlycybergams.Models;
+
+namespace quanlycybergams.Areas.Admin.Controllers
+{
+    public class DonHangsController : Controller
+    {
+        private QuanLyCYBERGAMESEntities db = new QuanLyCYBERGAMESEntities();
+
+        // GET: Admin/DonHangs
+        public ActionResult Index()
+        {
+            var donHangs = db.DonHangs.Include(d => d.DichVu).Include(d => d.TaiKhoan);
+            return View(donHangs.ToList());
+        }
+
+        // GET: Admin/DonHangs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // GET: Admin/DonHangs/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_DV = new SelectList(db.DichVus, "ID_DV", "TenDV");
+            ViewBag.ID_KhachHang = new SelectList(db.TaiKhoans, "ID_KhachHang", "TenKhachHang");
+            return View();
+        }
+
+        // POST: Admin/DonHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaDH,ID_DV,tongGia,ghiChu,ngayDatHang,ID_KhachHang")] DonHang donHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DonHangs.Add(donHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_DV = new SelectList(db.DichVus, "ID_DV", "TenDV", donHang.ID_DV);
+            ViewBag.ID_KhachHang = new SelectList(db.TaiKhoans, "ID_KhachHang", "TenKhachHang", donHang.ID_KhachHang);
+            return View(donHang);
+        }
+
+        // GET: Admin/DonHangs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_DV = new SelectList(db.DichVus, "ID_DV", "TenDV", donHang.ID_DV);
+            ViewBag.ID_KhachHang = new SelectList(db.TaiKhoans, "ID_KhachHang", "TenKhachHang", donHang.ID_KhachHang);
+            return View(donHang);
+        }
+
+        // POST: Admin/DonHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaDH,ID_DV,tongGia,ghiChu,ngayDatHang,ID_KhachHang")] DonHang donHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donHang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_DV = new SelectList(db.DichVus, "ID_DV", "TenDV", donHang.ID_DV);
+            ViewBag.ID_KhachHang = new SelectList(db.TaiKhoans, "ID_KhachHang", "TenKhachHang", donHang.ID_KhachHang);
+            return View(donHang);
+        }
+
+        // GET: Admin/DonHangs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // POST: Admin/DonHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            DonHang donHang = db.DonHangs.Find(id);
+            db.DonHangs.Remove(donHang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ quanlycybergams/quanlycybergams/Areas/Admin/Controllers/HoaDonsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using quanlycybergams.Models;
+
+namespace quanlycybergams.Areas.Admin.Controllers
+{
+    public class HoaDonsController : Controller
+    {
+        private QuanLyCYBERGAMESEntities db = new QuanLyCYBERGAMESEntities();
+
+        // GET: Admin/HoaDons
+        public ActionResult Index()
+        {
+            var hoaDons = db.HoaDons.Include(h => h.TaiKhoan).Include(h => h.May).Include(h => h.ThanhToanDV);
+            return View(hoaDons.ToList());
+        }
+
+        // GET: Admin/HoaDons/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoaDon hoaDon = db.HoaDons.Find(id);
+            if (hoaDon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hoaDon);
+        }
+
+        // GET: Admin/HoaDons/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_KhachHang = new SelectList(db.TaiKhoans, "ID_KhachHang", "TenKhachHang");
+            ViewBag.ID_May = new SelectList(db.Mays, "ID_May", "TenMay");
+            ViewBag.ID_TTDV = new SelectList(db.ThanhToanDVs, "ID_TTDV", "ID_KhachHang");
+            return View();
+        }
+
+        // POST: Admin/HoaDons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_HoaDon,ID_TTDV,ID_May,ThoiGianMo,ThoiGianTat,TongTien,TinhTrangHD,ID_KhachHang")] HoaDon hoaDon)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HoaDons.Add(hoaDon);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_KhachHang = new SelectList(db.TaiKhoans, "ID_KhachHang", "TenKhachHang", hoaDon.ID_KhachHang);
+            ViewBag.ID_May = new SelectList(db.Mays, "ID_May", "TenMay", hoaDon.ID_May);
+            ViewBag.ID_TTDV = new SelectList(db.ThanhToanDVs, "ID_TTDV", "ID_KhachHang", hoaDon.ID_TTDV);
+            return View(hoaDon);
+        }
+
+        // GET: Admin/HoaDons/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoaDon hoaDon = db.HoaDons.Find(id);
+            if (hoaDon == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_KhachHang = new SelectList(db.TaiKhoans, "ID_KhachHang", "TenKhachHang", hoaDon.ID_KhachHang);
+            ViewBag.ID_May = new SelectList(db.Mays, "ID_May", "TenMay", hoaDon.ID_May);
+            ViewBag.ID_TTDV = new SelectList(db.ThanhToanDVs, "ID_TTDV", "ID_KhachHang", hoaDon.ID_TTDV);
+            return View(hoaDon);
+        }
+
+        // POST: Admin/HoaDons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_HoaDon,ID_TTDV,ID_May,ThoiGianMo,ThoiGianTat,TongTien,TinhTrangHD,ID_KhachHang")] HoaDon hoaDon)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hoaDon).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_KhachHang = new SelectList(db.TaiKhoans, "ID_KhachHang", "TenKhachHang", hoaDon.ID_KhachHang);
+            ViewBag.ID_May = new SelectList(db.Mays, "ID_May", "TenMay", hoaDon.ID_May);
+            ViewBag.ID_TTDV = new SelectList(db.ThanhToanDVs, "ID_TTDV", "ID_KhachHang", hoaDon.ID_TTDV);
+            return View(hoaDon);
+        }
+
+        // GET: Admin/HoaDons/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoaDon hoaDon = db.HoaDons.Find(id);
+            if (hoaDon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hoaDon);
+        }
+
+        // POST: Admin/HoaDons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            HoaDon hoaDon = db.HoaDons.Find(id);
+            db.HoaDons.Remove(hoaDon);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
